fc23fea4e3b7a33347436da5c5f073badacf479d
Tarang Kalariya <tarangkalaria55@gmail.com>
2025-09-13 01:25:53 +0530

    added stream keys

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -4,6 +4,7 @@ declare namespace NodeJS {
 	interface ProcessEnv {
 		readonly NODE_ENV: "development" | "production" | "test";
 		readonly TZ?: string;
+
 		// clerkjs
 		readonly NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: string;
 		readonly CLERK_SECRET_KEY: string;
@@ -11,8 +12,14 @@ declare namespace NodeJS {
 		// Deployment used by `npx convex dev`
 		readonly CONVEX_DEPLOYMENT: string;
 		readonly NEXT_PUBLIC_CONVEX_URL: string;
+
+		// This needs to be added to convex
 		readonly CLERK_JWT_ISSUER_DOMAIN: string;
-		readonly CLERK_PUBLISHABLE_KEY: string;
-		readonly NEXT_PUBLIC_CLERK_FRONTEND_API_URL: string;
+		// readonly CLERK_PUBLISHABLE_KEY:string
+		// readonly NEXT_PUBLIC_CLERK_FRONTEND_API_URL:string
+
+		// Stream
+		readonly NEXT_PUBIC_STREAM_API_KEY: string;
+		readonly STREAM_API_SECRET_KEY: string;
 	}
 }
